5872fc7678c94d7c44f3795504bd665c66917a7f
Rafael <rdelarosaregana@gmail.com>
2025-12-27 12:45:40 +0100

    Primera version del securityConfiguration

---
+++ src/main/java/com/example/SpringSecurity/Customer/CustomerController1.java
@@ -0,0 +1,59 @@
+package com.example.SpringSecurity.Customer;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.session.SessionInformation;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class CustomerController1 {
+
+    @Autowired
+    private SessionRegistry sessionRegistry;
+
+    
+    @GetMapping("/index")
+    public String getCustomer() {
+        return "Hello Customer!";
+    }
+
+    @GetMapping("/index2")
+    public String index2() {
+        return "Hello world from index2!";
+    }
+
+    @GetMapping("/session")
+    public ResponseEntity<?> getDetailSession(){
+
+        String sessionId = "";
+        User userObject = null;
+
+        List<Object> sessions = sessionRegistry.getAllPrincipals();
+
+        for (Object session:sessions){
+            if(session instanceof User){
+                userObject = (User) session;
+            }
+
+            List<SessionInformation> sessionInformations = sessionRegistry.getAllSessions(session, false);
+            for(SessionInformation sessionInformation:sessionInformations){
+                sessionId= sessionInformation.getSessionId();
+            }
+        }
+
+        Map<String,Object> response = new HashMap<>();
+        response.put("response", "Hello World");
+        response.put("sessionId", sessionId);
+        response.put("sessionUser", userObject);
+        return (ResponseEntity<?>) ResponseEntity.ok(response);
+    }
+}
---
+++ src/main/java/com/example/SpringSecurity/config/SecurityConfiguration.java
@@ -0,0 +1,52 @@
+package com.example.SpringSecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+@Configuration
+@EnableWebSecurity //Configurar seguridad de nuestra aplicacion
+public class SecurityConfiguration {
+
+    //Configuration One (con funciones lambda)
+
+    @Bean
+    //Interfaz de Spring Security para configurar la seguridad
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception { 
+        return httpSecurity
+                //.csrf() //Si lo ponemos --> Inhabilitamos la seguridad que ofrece Spring frente a CSRF
+                .authorizeHttpRequests(authz -> authz
+                    .requestMatchers("/v1/index2").permitAll() //Endpoints que no van a necesitar ninguna autorización
+                    .anyRequest().authenticated()
+                )
+                .formLogin(frm -> frm.permitAll()
+                            .successHandler(successHandler())) //URL a donde se redirige de iniciar sesión
+
+                .sessionManagement(sm->sm
+                                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS) 
+                                .invalidSessionUrl("/login")
+                                .maximumSessions(1)
+                                .expiredUrl("/login") //Redireccion cuando expira la sesión
+                                .sessionRegistry(sessionRegistry())) 
+                //.httpBasic() //Enviamos nuestro usuario y contraseña en el HEADER de la petición
+                                                
+                .build();
+    }
+
+    @Bean
+    public SessionRegistry sessionRegistry(){
+        return new SessionRegistryImpl();
+    }
+    
+    public AuthenticationSuccessHandler successHandler(){ //Esto es lo que sucede si se inicia sesión exitosamente 
+        return ((request, response, authentication)->{
+            response.sendRedirect("/v1/session");
+        });
+    }
+}
